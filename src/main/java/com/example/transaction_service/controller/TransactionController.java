6dc785e0a1424f728d64da8b585751f1823a3055
Bienvenu2004 <kidsglory1@gmail.com>
2025-05-27 15:41:42 +0100

    feat: added hello endpoint

---
+++ src/main/java/com/example/transaction_service/controller/TransactionController.java
@@ -0,0 +1,19 @@
+package com.example.transaction_service.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/transaction")
+@CrossOrigin(origins = "*")
+public class TransactionController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from transaction Service!";
+    }
+}
