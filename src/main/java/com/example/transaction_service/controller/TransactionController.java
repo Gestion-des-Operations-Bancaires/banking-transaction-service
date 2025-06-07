132eba809e49690b3539612d61879b76d21982d0
Bienvenu2004 <kidsglory1@gmail.com>
2025-06-07 15:16:42 +0100

    added swagger

--- src/main/java/com/example/transaction_service/controller/TransactionController.java
+++ src/main/java/com/example/transaction_service/controller/TransactionController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/api/transaction")
+@RequestMapping("/transaction")
 @CrossOrigin(origins = "*")
 public class TransactionController {
 
